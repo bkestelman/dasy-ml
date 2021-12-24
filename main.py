8eabb73ed14fefe2c25c145e7b33738207a5f47a
Benito Kestelman <bkestelman3@gatech.edu>
2021-12-24 17:08:52 -0500

    CentroidsLabeler

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from synthesizers.gaussian import GaussianSynth
+from labelers.centroids import CentroidsLabeler
 
 def main():
+    dim = 2
+
     synth = GaussianSynth()
-    data = synth.sample(n=100, dim=2)
-    plt.scatter(data.T[0], data.T[1])
+    data = synth.sample(n=100, dim=dim)
+    #plt.scatter(data.T[0], data.T[1])
+    #plt.show()
+
+    labeler = CentroidsLabeler(classes=2, dim=dim)
+    labels = labeler.assign(data)
+    plt.scatter(data.T[0], data.T[1], c=labels)
     plt.show()
 
 if __name__ == '__main__':
