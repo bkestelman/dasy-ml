cd3bb6f45d9449d869c6f43e56bda5ad9d3d23b7
Benito Kestelman <bkestelman3@gatech.edu>
2021-12-24 18:36:37 -0500

    show centroids and linear separator

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from labelers.centroids import CentroidsLabeler
 
 def main():
     dim = 2
+    plt.gca().set_aspect('equal', adjustable='box')
 
     synth = GaussianSynth()
     data = synth.sample(n=100, dim=dim)
@@ -14,7 +15,17 @@ def main():
     labeler = CentroidsLabeler(classes=2, dim=dim)
     labels = labeler.assign(data)
     plt.scatter(data.T[0], data.T[1], c=labels)
+    plt.scatter(labeler.centroids.T[0], labeler.centroids.T[1], c='r')
+    plt.plot(labeler.centroids.T[0], labeler.centroids.T[1], '--')
+    plot_bisectors(labeler.centroids[0], labeler.centroids[1])
+    plt.title('Gaussian Data with Centroid Labeling')
     plt.show()
+
+def plot_bisectors(a, b):
+    midpoint = (a + b) / 2
+    slope = - (b[0] - a[0]) / (b[1] - a[1])
+    #plt.plot([midpoint[0], midpoint[0] + 1], [midpoint[1], midpoint[1] + slope], '-')
+    plt.axline((midpoint[0], midpoint[1]), slope=slope, linestyle='--')
 
 if __name__ == '__main__':
     main()
