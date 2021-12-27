2d48b6d0027804f7e3c4b936069a84ef62e792fb
Benito Kestelman <bkestelman3@gatech.edu>
2021-12-27 16:52:00 -0500

    change default interval

--- dasy/synthesizers/uniform.py
+++ dasy/synthesizers/uniform.py
@@ -7,6 +7,6 @@ class UniformSynth(DataSynthesizer):
     """
     Samples from a single Uniform Distribution on an interval
     """
-    def sample(self, n=100, dim=2, low=-1., high=1.):
+    def sample(self, n=100, dim=2, low=-10., high=10.):
         return np.random.uniform(low=low, high=high, size=(n, dim))
 
