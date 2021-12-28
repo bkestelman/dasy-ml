2633cbb879e2569b9dba06ca4e325bcfc4f26994
Benito Kestelman <bkestelman3@gatech.edu>
2021-12-27 20:19:07 -0500

    clear old plot

--- tests/test_regression_labelers.py
+++ tests/test_regression_labelers.py
@@ -11,8 +11,11 @@ class TestRegressionLabelers(unittest.TestCase):
         labeler = LinearRegressionLabeler(dim=dim)
         X = synth.sample(n=n, dim=dim)
         y = labeler.assign(X)
+        #print(X.shape)
+        #print(y.shape)
 
-        plt.scatter(X, y)
+        plt.clf()
+        plt.scatter(X.T[0], y)
         plt.axline((0, labeler.b), slope=labeler.w, color='r')
         plt.title('Uniform Data x with Linear y + noise')
         plt.savefig('linear_labeler')
